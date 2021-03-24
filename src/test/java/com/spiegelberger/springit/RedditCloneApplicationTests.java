b19481922f017ea7de1bb59d7005adfd7fd0ac2f
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-03-24 13:47:47 +0100

    Initial commit

---
+++ src/test/java/com/spiegelberger/springit/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiegelberger.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
