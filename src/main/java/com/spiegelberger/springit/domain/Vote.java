23a5355fb1da6e0ef093dae30ef4f9179e6308e4
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-04-01 07:18:52 +0200

    Vote entity added

--- src/main/java/com/spiegelberger/springit/domain/Vote.java
+++ src/main/java/com/spiegelberger/springit/domain/Vote.java
@@ -3,17 +3,23 @@ package com.spiegelberger.springit.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Entity
 @Data
-public class Vote {
+public class Vote extends Auditable{
 
 	@Id
 	@GeneratedValue
 	private Long id;
 	
-	private int vote;
+	@NonNull
+	private short direction;
 	
+	@NonNull
+	@ManyToOne
+	private Link link;
 }
