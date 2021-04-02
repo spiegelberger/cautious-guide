e3e08aa28c44c7a61b0f1958d666021ec506339b
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-04-02 13:30:14 +0200

    LinkController refactored using LinkService

--- src/main/java/com/spiegelberger/springit/controller/LinkController.java
+++ src/main/java/com/spiegelberger/springit/controller/LinkController.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -19,35 +17,38 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.spiegelberger.springit.domain.Comment;
 import com.spiegelberger.springit.domain.Link;
 import com.spiegelberger.springit.repository.CommentRepository;
-import com.spiegelberger.springit.repository.LinkRepository;
+import com.spiegelberger.springit.service.LinkService;
+
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @Controller
 public class LinkController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
+//	private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 	
 	
-	private LinkRepository linkRepository;
+	private LinkService linkService;
 	private CommentRepository commentRepository;
 	
 	
 	@Autowired
-	public LinkController(LinkRepository linkRepository, CommentRepository commentRepository) {
-		this.linkRepository = linkRepository;
+	public LinkController(LinkService linkService, CommentRepository commentRepository) {
+		this.linkService = linkService;
 		this.commentRepository = commentRepository;
 	}
 
 
 	@GetMapping("/")
 	public String list(Model model) {
-		model.addAttribute("links", linkRepository.findAll());
+		model.addAttribute("links", linkService.findAll());
 		return "link/list";
 	}
 		
 	@GetMapping("/link/{id}")
 	public String read(@PathVariable Long id, Model model) {
-		Optional<Link> optionalLink = linkRepository.findById(id);
+		Optional<Link> optionalLink = linkService.findById(id);
 		if(optionalLink.isPresent()) {
 			Link currentLink = optionalLink.get();
 			
@@ -76,13 +77,13 @@ public class LinkController {
 	public String createLink(@Valid Link link, BindingResult bindingResult,
 					Model model, RedirectAttributes redirectAttributes) {
 		if(bindingResult.hasErrors()) {
-			logger.info("Validation errors were found while submitting a new link");
+			log.info("Validation errors were found while submitting a new link");
 			model.addAttribute("link", link);
 			return "link/submit";
 		}
 		else {
-			linkRepository.save(link);
-			logger.info("New Link was saved successfully.");
+			linkService.save(link);
+			log.info("New Link was saved successfully.");
 			redirectAttributes
 				.addAttribute("id", link.getId())
 				.addFlashAttribute("success", true);
@@ -95,10 +96,10 @@ public class LinkController {
 	public String addComment(@Valid Comment comment,BindingResult bindingResult,
 					Model model, RedirectAttributes redirectAttributes) {
 	    if( bindingResult.hasErrors() ) {
-	        logger.info("Something went wrong.");
+	        log.info("Something went wrong.");
 	    } 
 	    else {
-	        logger.info("New Comment was saved successfully!");
+	        log.info("New Comment was saved successfully!");
 	        commentRepository.save(comment);
 	    }
 	    return "redirect:/link/" + comment.getLink().getId();
---
+++ src/main/java/com/spiegelberger/springit/service/LinkService.java
@@ -0,0 +1,34 @@
+package com.spiegelberger.springit.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spiegelberger.springit.domain.Link;
+import com.spiegelberger.springit.repository.LinkRepository;
+
+@Service
+public class LinkService {
+
+	private LinkRepository linkRepository;
+
+	@Autowired
+	public LinkService(LinkRepository linkRepository) {
+		this.linkRepository = linkRepository;
+	}
+	
+	public List<Link>findAll(){
+		return linkRepository.findAll();
+	}
+	
+	public Optional<Link>findById(Long id){
+		return linkRepository.findById(id);
+	}
+	
+	public Link save(Link link) {
+		return linkRepository.save(link);
+	}
+	
+}
